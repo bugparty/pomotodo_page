dd22c98d6d2fffdd51eeb6dd697cebfe4e1ee3d3
bugparty <bugparty@users.noreply.github.com>
2020-07-06 09:10:45 +0000

    deploy: bugparty/tomatodoapp@ca17ee9fc6eac5494fefec498623163f45c2bfa8

---
+++ precache-manifest.bc2e62b6da9da27d470c87c734836189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7702d372ff3c8d87f7bb",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/css/main.951d709a.chunk.css"
+  },
+  {
+    "revision": "7702d372ff3c8d87f7bb",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/js/main.7702d372.chunk.js"
+  },
+  {
+    "revision": "c60c425b93e79017c91d",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/js/runtime~main.c60c425b.js"
+  },
+  {
+    "revision": "4de96711915194d63389",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/js/2.4de96711.chunk.js"
+  },
+  {
+    "revision": "8dfffc026adbf1840a0776f58d3c6f2d",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/index.html"
+  }
+];
