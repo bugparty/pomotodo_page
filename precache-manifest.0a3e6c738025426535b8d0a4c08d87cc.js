10f2fabb418863c99be804aca29a914715bce663
bugparty <bugparty@users.noreply.github.com>
2020-07-06 08:23:24 +0000

    deploy: bugparty/tomatodoapp@87475fa7ac03b8569502a074dc7379c36e8579ed

---
+++ precache-manifest.0a3e6c738025426535b8d0a4c08d87cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6ac146565274f242dc",
+    "url": "/static/css/main.1c21e8f1.chunk.css"
+  },
+  {
+    "revision": "1a6ac146565274f242dc",
+    "url": "/static/js/main.1a6ac146.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "4de96711915194d63389",
+    "url": "/static/js/2.4de96711.chunk.js"
+  },
+  {
+    "revision": "27d078cef09a10fcc941ce1475c764e2",
+    "url": "/index.html"
+  }
+];
