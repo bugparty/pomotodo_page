5d5827893c82c3b5cadf2d72d9b84685745f91e9
bugparty <bugparty@users.noreply.github.com>
2020-08-13 14:19:13 +0000

    deploy: bugparty/tomatodoapp@8479b871b65a82d0a1d22948d88b4df6a6abbfea

---
+++ precache-manifest.924e76cf7287a7b358f05497da6d96a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7044e9182f803a0b9732db4e9b3b1b5",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/index.html"
+  },
+  {
+    "revision": "c615c1ab48d74acd1728",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/css/2.5c600ed6.chunk.css"
+  },
+  {
+    "revision": "0fc1f8163af6df5312ce",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/css/main.951d709a.chunk.css"
+  },
+  {
+    "revision": "c615c1ab48d74acd1728",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/js/2.184c8c80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/js/2.184c8c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc1f8163af6df5312ce",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/js/main.c0a32b11.chunk.js"
+  },
+  {
+    "revision": "ab2cdb40c9bdfe5fedeb",
+    "url": "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/static/js/runtime-main.caac66c3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/precache-manifest.70f28d6ff0489c87b9c6fc8cb5ab9f19.js"
+  "https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/precache-manifest.924e76cf7287a7b358f05497da6d96a6.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://cdn.jsdelivr.net/gh/bugparty/pomotodo_page@gh-pages/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
